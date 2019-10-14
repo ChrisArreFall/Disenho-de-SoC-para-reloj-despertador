5be39ae6f50182a5f08af25750a8b84e039d97e2
Erick <erickm968@gmail.com>
2019-10-13 20:15:34 -0600

    NIOS SoC

---
+++ NIOS/NIOS_SoC/NIOS_SoC_inst.v
@@ -0,0 +1,15 @@
+	NIOS_SoC u0 (
+		.clk_clk       (<connected-to-clk_clk>),       //    clk.clk
+		.reset_reset_n (<connected-to-reset_reset_n>), //  reset.reset_n
+		.btn_export    (<connected-to-btn_export>),    //    btn.export
+		.hex0_export   (<connected-to-hex0_export>),   //   hex0.export
+		.hex1_export   (<connected-to-hex1_export>),   //   hex1.export
+		.hex2_export   (<connected-to-hex2_export>),   //   hex2.export
+		.hex3_export   (<connected-to-hex3_export>),   //   hex3.export
+		.hex4_export   (<connected-to-hex4_export>),   //   hex4.export
+		.hex5_export   (<connected-to-hex5_export>),   //   hex5.export
+		.timer_export  (<connected-to-timer_export>),  //  timer.export
+		.uartpc_rxd    (<connected-to-uartpc_rxd>),    // uartpc.rxd
+		.uartpc_txd    (<connected-to-uartpc_txd>)     //       .txd
+	);
+
---
+++ NIOS/software/Alarma/obj/default/hello_world_small.d
@@ -0,0 +1,4 @@
+obj/default/hello_world_small.o: hello_world_small.c \
+ ../Alarma_bsp//HAL/inc/sys/alt_stdio.h
+
+../Alarma_bsp//HAL/inc/sys/alt_stdio.h:
